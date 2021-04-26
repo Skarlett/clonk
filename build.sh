ffb1d9f6e91fc7ef5735fe857df29185ba1534a0
Skarlett <zykl@protonmail.com>
2021-04-26 09:45:18 -0500

    introducing new files

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+gcc -ggdb -o a.out src/main.c src/core/*.c
---
+++ src/core/syn.c
@@ -0,0 +1,27 @@
+//synthesizer
+// maintains sanity by constructing some rules
+// in which the interpreter expects
+
+#include <stdlib.h>
+#include "lexer.h"
+#include "expr.h"
+#include "ast.h"
+
+
+void synthesize(BlockStatement *block) {
+    for (size_t i=0; block->length > i; i++) {
+        
+    }
+}
+
+
+// `if` + `block`
+
+// `define` + `block`
+
+// `expr` + `operator` + `expr`
+
+// `define` + `block` + `return`
+
+// ! `block` + `return`
+
