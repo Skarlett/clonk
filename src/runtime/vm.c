eda37d425df1d79a4f349b6a663653d49e78df9a
Skarlett <360d@pm.me>
2021-05-08 10:18:03 -0500

    Mass rename of fields in `Registers`

--- src/runtime/vm.c
+++ src/runtime/vm.c
@@ -14,10 +14,16 @@ void init_vm(VM *vm,
     u_int64_t code_sz,
     u_int64_t i32_const_sz    
 ){
-    vm->registers.fp=0;
-    vm->registers.sp=0;
-    vm->registers.ip=0;
-    vm->registers.rp=0;
+    vm->registers.rsp=0;
+    vm->registers.rbp=0;
+    vm->registers.rip=0;
+    
+    vm->registers.rax=0;
+    vm->registers.rbx=0;
+    
+    vm->registers.rcx=0;
+    vm->registers.rdi=0;
+
     vm->registers.trap=0;
     vm->registers.trace=0;
 
@@ -39,91 +45,91 @@ void init_vm(VM *vm,
 void run_instruction(enum Opcode op, VM *vm) {
     switch (op) {
         case i32Push:
-            memcpy(vm->stack + vm->registers.sp, vm->code + vm->registers.ip, sizeof(int32_t));
-            vm->registers.sp += sizeof(int32_t);
-            vm->registers.ip += sizeof(int32_t);
+            memcpy(vm->stack + vm->registers.rsp, vm->code + vm->registers.rip, sizeof(int32_t));
+            vm->registers.rsp += sizeof(int32_t);
+            vm->registers.rip += sizeof(int32_t);
             break;
 
         case i32Add:
-            vm->registers.rax=(int32_t)*(vm->stack + vm->registers.sp);
-            vm->registers.rbx=(int32_t)*(vm->stack + vm->registers.sp - sizeof(int32_t));
+            vm->registers.rax=(int32_t)*(vm->stack + vm->registers.rsp);
+            vm->registers.rbx=(int32_t)*(vm->stack + vm->registers.rsp - sizeof(int32_t));
             
             memcpy(
-                vm->stack + vm->registers.sp - sizeof(int32_t), 
+                vm->stack + vm->registers.rsp - sizeof(int32_t), 
                 (void *)((int32_t)vm->registers.rax + (int32_t)vm->registers.rbx),
                 sizeof(int32_t)
             );
 
-            vm->registers.sp -= sizeof(int32_t);
+            vm->registers.rsp -= sizeof(int32_t);
             break;
         
         case i32Div:
-            vm->registers.rax=(int32_t)*(vm->stack + vm->registers.sp);
-            vm->registers.rbx=(int32_t)*(vm->stack + vm->registers.sp - sizeof(int32_t));
+            vm->registers.rax=(int32_t)*(vm->stack + vm->registers.rsp);
+            vm->registers.rbx=(int32_t)*(vm->stack + vm->registers.rsp - sizeof(int32_t));
             
             memcpy(
-                vm->stack + vm->registers.sp - sizeof(int32_t), 
+                vm->stack + vm->registers.rsp - sizeof(int32_t), 
                 (int32_t *)((int32_t)vm->registers.rax / (int32_t)vm->registers.rbx),
                 sizeof(int32_t)
             );
 
-            vm->registers.sp -= sizeof(int32_t);
+            vm->registers.rsp -= sizeof(int32_t);
             break;
         
         case i32Sub:
-            vm->registers.rax=(int32_t)*(vm->stack + vm->registers.sp);
-            vm->registers.rbx=(int32_t)*(vm->stack + vm->registers.sp - sizeof(int32_t));
+            vm->registers.rax=(int32_t)*(vm->stack + vm->registers.rsp);
+            vm->registers.rbx=(int32_t)*(vm->stack + vm->registers.rsp - sizeof(int32_t));
             
             memcpy(
-                vm->stack + vm->registers.sp - sizeof(int32_t), 
+                vm->stack + vm->registers.rsp - sizeof(int32_t), 
                 (int32_t *)((int32_t)vm->registers.rax - (int32_t)vm->registers.rbx),
                 sizeof(int32_t)
             );
 
-            vm->registers.sp -= sizeof(int32_t);
+            vm->registers.rsp -= sizeof(int32_t);
             break;
         
         case i32Pow:
-            vm->registers.rax=(int32_t)*(vm->stack + vm->registers.sp);
-            vm->registers.rbx=(int32_t)*(vm->stack + vm->registers.sp - sizeof(int32_t));
+            vm->registers.rax=(int32_t)*(vm->stack + vm->registers.rsp);
+            vm->registers.rbx=(int32_t)*(vm->stack + vm->registers.rsp - sizeof(int32_t));
             
             memcpy(
-                vm->stack + vm->registers.sp - sizeof(int32_t), 
+                vm->stack + vm->registers.rsp - sizeof(int32_t), 
                 (int32_t *)((int32_t)vm->registers.rax ^ (int32_t)vm->registers.rbx),
                 sizeof(int32_t)
             );
 
-            vm->registers.sp -= sizeof(int32_t);
+            vm->registers.rsp -= sizeof(int32_t);
             break;
 
         case i32Mul:
-            vm->registers.rax=(int32_t)*(vm->stack + vm->registers.sp);
-            vm->registers.rbx=(int32_t)*(vm->stack + vm->registers.sp - sizeof(int32_t));
+            vm->registers.rax=(int32_t)*(vm->stack + vm->registers.rsp);
+            vm->registers.rbx=(int32_t)*(vm->stack + vm->registers.rsp - sizeof(int32_t));
             
             memcpy(
-                vm->stack + vm->registers.sp - sizeof(int32_t), 
+                vm->stack + vm->registers.rsp - sizeof(int32_t), 
                 (int32_t *)(vm->registers.rax * vm->registers.rbx),
                 sizeof(int32_t)
             );
 
-            vm->registers.sp -= sizeof(int32_t);
+            vm->registers.rsp -= sizeof(int32_t);
             break;
 
             break;
         // [1, 2]
         // []
         case i32Modolus:
-            vm->registers.rax=(int32_t)*(vm->stack + vm->registers.sp);
-            vm->registers.rbx=(int32_t)*(vm->stack + vm->registers.sp - sizeof(int32_t));
+            vm->registers.rax=(int32_t)*(vm->stack + vm->registers.rsp);
+            vm->registers.rbx=(int32_t)*(vm->stack + vm->registers.rsp - sizeof(int32_t));
             
 
             memcpy(
-                vm->stack + vm->registers.sp - sizeof(int32_t), 
+                vm->stack + vm->registers.rsp - sizeof(int32_t), 
                 (int32_t *)((int32_t)vm->registers.rax % (int32_t)vm->registers.rbx),
                 sizeof(int32_t)
             );
 
-            vm->registers.sp -= sizeof(int32_t);
+            vm->registers.rsp -= sizeof(int32_t);
             break;
 
     
@@ -133,38 +139,38 @@ void run_instruction(enum Opcode op, VM *vm) {
         // [1]
         case i32Gt:
             // store top staclitem
-            vm->registers.rax = (int32_t)*(vm->stack + vm->registers.sp);
-            vm->registers.rbx = (int32_t)*(vm->stack + vm->registers.sp - sizeof(int32_t));
-            vm->registers.sp -= sizeof(int32_t)*2;
-            vm->code[vm->registers.sp++] = (char)(vm->registers.rax > vm->registers.rbx);
+            vm->registers.rax = (int32_t)*(vm->stack + vm->registers.rsp);
+            vm->registers.rbx = (int32_t)*(vm->stack + vm->registers.rsp - sizeof(int32_t));
+            vm->registers.rsp -= sizeof(int32_t)*2;
+            vm->code[vm->registers.rsp++] = (char)(vm->registers.rax > vm->registers.rbx);
             break;
         
         case i32GtEq:
-            vm->registers.rax=(int32_t)*(vm->stack + vm->registers.sp);
-            vm->registers.rbx=(int32_t)*(vm->stack + vm->registers.sp - sizeof(int32_t));
-            vm->registers.sp -= sizeof(int32_t)*2;
-            vm->code[vm->registers.sp++] = (char)(vm->registers.rax >= vm->registers.rbx);
+            vm->registers.rax=(int32_t)*(vm->stack + vm->registers.rsp);
+            vm->registers.rbx=(int32_t)*(vm->stack + vm->registers.rsp - sizeof(int32_t));
+            vm->registers.rsp -= sizeof(int32_t)*2;
+            vm->code[vm->registers.rsp++] = (char)(vm->registers.rax >= vm->registers.rbx);
             break;
         
         case i32IsEq:
-            vm->registers.rax=(int32_t)*(vm->stack + vm->registers.sp);
-            vm->registers.rbx=(int32_t)*(vm->stack + vm->registers.sp - sizeof(int32_t));
-            vm->registers.sp -= sizeof(int32_t)*2;
-            vm->code[vm->registers.sp++] = (char)(vm->registers.rax == vm->registers.rbx);
+            vm->registers.rax=(int32_t)*(vm->stack + vm->registers.rsp);
+            vm->registers.rbx=(int32_t)*(vm->stack + vm->registers.rsp - sizeof(int32_t));
+            vm->registers.rsp -= sizeof(int32_t)*2;
+            vm->code[vm->registers.rsp++] = (char)(vm->registers.rax == vm->registers.rbx);
             break;
         
         case i32Lt:
-            vm->registers.rax=(int32_t)*(vm->stack + vm->registers.sp);
-            vm->registers.rbx=(int32_t)*(vm->stack + vm->registers.sp - sizeof(int32_t));
-            vm->registers.sp -= sizeof(int32_t)*2;
-            vm->code[vm->registers.sp++] = (char)(vm->registers.rax < vm->registers.rbx);
+            vm->registers.rax=(int32_t)*(vm->stack + vm->registers.rsp);
+            vm->registers.rbx=(int32_t)*(vm->stack + vm->registers.rsp - sizeof(int32_t));
+            vm->registers.rsp -= sizeof(int32_t)*2;
+            vm->code[vm->registers.rsp++] = (char)(vm->registers.rax < vm->registers.rbx);
             break;
 
         case i32LtEq:
-            vm->registers.rax = (int32_t)*(vm->stack + vm->registers.sp);
-            vm->registers.rbx = (int32_t)*(vm->stack + vm->registers.sp - sizeof(int32_t));
-            vm->registers.sp -= sizeof(int32_t)*2;
-            vm->code[vm->registers.sp++] = (char)(vm->registers.rax <= vm->registers.rbx);
+            vm->registers.rax = (int32_t)*(vm->stack + vm->registers.rsp);
+            vm->registers.rbx = (int32_t)*(vm->stack + vm->registers.rsp - sizeof(int32_t));
+            vm->registers.rsp -= sizeof(int32_t)*2;
+            vm->code[vm->registers.rsp++] = (char)(vm->registers.rax <= vm->registers.rbx);
             break;
         
         case Halt:
@@ -180,37 +186,37 @@ void run_instruction(enum Opcode op, VM *vm) {
             break;
 
         case Goto: // expects u64
-            vm->registers.ip = (u_int64_t)*(vm->code + vm->registers.ip + sizeof(Goto));
+            vm->registers.rip = (u_int64_t)*(vm->code + vm->registers.rip + sizeof(Goto));
             break;
         
         // addr
         // 1
         case GotoT:
             // flag
-            vm->registers.rax = (char)*(vm->code + vm->registers.ip + sizeof(GotoT));
+            vm->registers.rax = (char)*(vm->code + vm->registers.rip + sizeof(GotoT));
             
             if (vm->registers.rax > 0)
-                vm->registers.ip = (u_int64_t)*(vm->code + vm->registers.ip + sizeof(GotoT) + sizeof(char));
+                vm->registers.rip = (u_int64_t)*(vm->code + vm->registers.rip + sizeof(GotoT) + sizeof(char));
             else
-                vm->registers.ip += sizeof(GotoT) + sizeof(char) + sizeof(u_int64_t);
+                vm->registers.rip += sizeof(GotoT) + sizeof(char) + sizeof(u_int64_t);
             break;
                     
         case GotoF:
-            vm->registers.rax = (char)*(vm->code + vm->registers.ip + sizeof(GotoT));
+            vm->registers.rax = (char)*(vm->code + vm->registers.rip + sizeof(GotoT));
             
             if (vm->registers.rax > 0)
-                vm->registers.ip = (u_int64_t)*(vm->code + vm->registers.ip + sizeof(GotoT) + sizeof(char));
+                vm->registers.rip = (u_int64_t)*(vm->code + vm->registers.rip + sizeof(GotoT) + sizeof(char));
             else
-                vm->registers.ip += sizeof(GotoT) + sizeof(char) + sizeof(u_int64_t);
+                vm->registers.rip += sizeof(GotoT) + sizeof(char) + sizeof(u_int64_t);
             break;
         
         case i32Inc:
-            vm->registers.rax=(u_int32_t *)&vm->stack[vm->registers.sp];
+            vm->registers.rax=(u_int32_t *)&vm->stack[vm->registers.rsp];
             vm->registers.rax += 1;
             break;
         
         case i32Dec:
-            vm->registers.rax=(u_int32_t *)&vm->stack[vm->registers.sp];
+            vm->registers.rax=(u_int32_t *)&vm->stack[vm->registers.rsp];
             vm->registers.rax -= 1;
             break;
         
@@ -251,24 +257,24 @@ const char * popcode(enum Opcode op) {
 int operands_size(enum Opcode op) {
     switch (op) {
         case Pop: return 1;
-        case i32Push: return sizeof(int)*2;
-        case i32Add: return sizeof(int)*2;
-        case i32Div: return sizeof(int)*2;
-        case i32Sub: return sizeof(int)*2;
-        case i32Pow: return sizeof(int)*2;
-        case i32Mul: return sizeof(int)*2;
-        case i32Modolus: return sizeof(int)*2;
-        case i32Gt: return sizeof(int)*2;
-        case i32GtEq: return sizeof(int)*2;
-        case i32IsEq: return sizeof(int)*2;
-        case i32Lt: return sizeof(int)*2;
-        case i32LtEq: return sizeof(int)*2;
+        case i32Push: return sizeof(u_int32_t)*2;
+        case i32Add: return sizeof(u_int32_t)*2;
+        case i32Div: return sizeof(u_int32_t)*2;
+        case i32Sub: return sizeof(u_int32_t)*2;
+        case i32Pow: return sizeof(u_int32_t)*2;
+        case i32Mul: return sizeof(u_int32_t)*2;
+        case i32Modolus: return sizeof(u_int32_t)*2;
+        case i32Gt: return sizeof(u_int32_t)*2;
+        case i32GtEq: return sizeof(u_int32_t)*2;
+        case i32IsEq: return sizeof(u_int32_t)*2;
+        case i32Lt: return sizeof(u_int32_t)*2;
+        case i32LtEq: return sizeof(u_int32_t)*2;
         case Halt: return 0;
-        case Print: return sizeof(size_t);
-        case Ret: return sizeof(size_t);
-        case Goto: return sizeof(size_t);
-        case GotoT:return sizeof(size_t)+sizeof(char);
-        case GotoF: return sizeof(size_t)+sizeof(char);
+        case Print: return sizeof(u_int32_t);
+        case Ret: return sizeof(u_int32_t);
+        case Goto: return sizeof(u_int32_t);
+        case GotoT:return sizeof(u_int32_t)+sizeof(char);
+        case GotoF: return sizeof(u_int32_t)+sizeof(char);
         case ISANop: return 0;
         default: return -1;
     }
@@ -277,13 +283,13 @@ int operands_size(enum Opcode op) {
 
 void run_vm(VM *vm) {
 
-    while(vm->code_len >= vm->registers.ip) {
-        enum Opcode opcode = vm->code[vm->registers.ip];
+    while(vm->code_len >= vm->registers.rip) {
+        enum Opcode opcode = vm->code[vm->registers.rip];
         run_instruction(opcode, vm);
         
         if (vm->registers.trace > 0)
-            printf("%lu%s", vm->registers.ip, popcode(opcode));
+            printf("%lu%s", vm->registers.rip, popcode(opcode));
             
-        vm->registers.ip++;
+        vm->registers.rip++;
     }
 }
