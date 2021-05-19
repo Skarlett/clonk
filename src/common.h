42da8e2f9af8e978708c23bd699c933a3de6831b
Skarlett <zykl@protonmail.com>
2021-05-19 11:49:15 -0500

    change `size_t` to `uint32_t`

--- src/common.h
+++ src/common.h
@@ -4,14 +4,14 @@
 #define VERSION "0.0.1"
 
 #include <stdlib.h>
-
+#include <stdint.h>
 #define TRUE 1
 #define FALSE 0
 
 
 
-void * xmalloc(size_t size);
-void * xrealloc(size_t size);
-void * tab_print(size_t size);
+void * xmalloc(uint32_t size);
+void * xrealloc(uint32_t size);
+void * tab_print(uint32_t size);
 
 #endif
