acad3bcdc1add353b040e36ca6942cc7c69fa819
Skarlett <360d@pm.me>
2021-09-01 02:46:30 -0500

    add `inline` to conditional functions - can't believe I forgot about this keyword.

--- src/parser/lexer/helpers.h
+++ src/parser/lexer/helpers.h
@@ -1,14 +1,16 @@
 #ifndef _HEADER__LEXER_HELPER__
 #define _HEADER__LEXER_HELPER__
 
-enum Lexicon invert_brace_type(enum Lexicon token);
 const char * ptoken(enum Lexicon t);
-int is_cmp_operator(enum Lexicon compound_token);
-int is_bin_operator(enum Lexicon compound_token);
-int is_close_brace(enum Lexicon token);
-int is_open_brace(enum Lexicon token); 
-int is_data(enum Lexicon token);
+inline enum Lexicon invert_brace_type(enum Lexicon token);
+inline int is_cmp_operator(enum Lexicon compound_token);
+inline int is_bin_operator(enum Lexicon compound_token);
+inline int is_close_brace(enum Lexicon token);
+inline int is_open_brace(enum Lexicon token); 
+inline int is_data(enum Lexicon token);
+inline int is_utf(char ch);
+
+int is_keyword(enum Lexicon token);
 int is_balanced(struct Token tokens[], unsigned long int ntokens);
 int is_balanced_by_ref(struct Token *tokens[], unsigned long int ntokens);
-
 #endif
