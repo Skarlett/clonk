3a143b92e185fc5dc1c94e5b8f7435e45df95f81
Skarlett <360d@pm.me>
2021-09-01 08:56:35 -0500

    deprecate `Expr` in favor of new implementation. comment out all the deprecated code that uses our old `Expr` structure

--- src/parser/expr/debug.h
+++ src/parser/expr/debug.h
@@ -3,9 +3,9 @@
 
 #include "expr.h"
 
-void ptree(Expr *expr);
+//void ptree(struct Expr *expr);
 void draw_token_error_at(char * line, struct Token *token);
-int print_expr(Expr *expr, short unsigned indent);
+//int print_expr(struct Expr *expr, short unsigned indent);
 const char * p_bin_operator_sym(enum BinOp t);
 const char * print_bin_operator(enum BinOp t);
 const char * print_expr_t(enum ExprType t);
