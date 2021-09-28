370d47f9f44e8fbbb8c844e9088642400ef6d1d3
omitted <omitted>
2021-09-27 20:17:52 -0500

    trim unused code

--- src/parser/expr/helpers.h
+++ src/parser/expr/helpers.h
@@ -2,13 +2,13 @@
 #define _HEADER_EXPR_HELPER__
 #include <stdint.h>
 #include "../../prelude.h"
-
+#include "../lexer/lexer.h"
 
 // int cmpexpr(struct Expr *a, struct  Expr *b);
 // int is_expr_token(enum Lexicon token);
 // int is_expr(char *line, struct Token tokens[], size_t ntokens);
 // //size_t get_expr_len(Expr *expr);
-
+int8_t binop_from_token(enum Lexicon t);
 // int symbol_from_token(const char *line, struct Token token, struct Symbol *value);
 
 
