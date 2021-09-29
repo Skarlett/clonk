a510e7ba1be3aa2eaf323454159f39750de3c277
omitted <omitted>
2021-09-28 21:59:39 -0500

    adds `AssertTokens`

--- tests/common.c
+++ tests/common.c
@@ -1,22 +1,37 @@
+#include "CuTest.h"
 #include "../src/parser/lexer/lexer.h"
+#include "../src/parser/lexer/debug.h"
+#include "../src/parser/lexer/helpers.h"
 #include "../src/prelude.h"
+#include <stdio.h>
 
 int __check_tokens(struct Token tokens[], enum Lexicon lexicon[], usize len){
     for (int i=0; len > i; i++) {
         if (tokens[i].type != lexicon[i]) {
-            return -1;
+            return 0;
         }
     }
-    return 0;
+    return 1;
 }
-
 
 int __check_tokens_by_ref(struct Token *tokens[], enum Lexicon lexicon[], usize len){
-    for (int i=0; len > i; i++) {
+    for (usize i=0; len > i; i++) {
         if (tokens[i]->type != lexicon[i]) {
-            return -1;
+            return 0;
         }
     }
-    return 0;
+
+    return 1;
 }
 
+void AssertTokens(CuTest *tc, struct Token tokens[], enum Lexicon answer[], usize len) {
+    char msg[2048];
+    char got[512];
+    char expected[512];
+
+    sprintf_token_slice(tokens, len, got, 512);
+    sprintf_lexicon_slice(answer, len, expected, 512);
+    sprintf(msg, "expected: \n%s\ngot: \n%s\n", expected, got);
+
+    CuAssert(tc, msg, __check_tokens(tokens, answer, len) == 0);
+}
--- tests/common.h
+++ tests/common.h
@@ -1,9 +1,11 @@
 #ifndef _HEADER__COMMON_TESTS_HELPER__
 #define _HEADER__COMMON_TESTS_HELPER__
-
+#include "CuTest.h"
 #include "../src/prelude.h"
 #include "../src/parser/lexer/lexer.h"
+
 int __check_tokens(struct Token tokens[], enum Lexicon lexicon[], usize len);
 int __check_tokens_by_ref(struct Token *tokens[], enum Lexicon lexicon[], usize len);
 
+void AssertTokens(CuTest *tc, struct Token tokens[], enum Lexicon lexicon[], usize len);
 #endif
