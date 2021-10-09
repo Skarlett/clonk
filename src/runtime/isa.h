d1fcbc705fd3917f532a2e3f10e54110cc21120e
Skarlett <360d@pm.me>
2021-10-08 21:40:22 -0500

    Considering a rust bytecode-VM

--- src/runtime/isa.h
+++ src/runtime/isa.h
@@ -1,24 +1,20 @@
 enum Opcode {
     ISANop,
+
     i64Const,
-    
     i64Push,
     i64IsEq,
     i64Gt,
     i64Lt,
     i64GtEq,
     i64LtEq,
-
     i64Add,
     i64Sub,
     i64Div,
     i64Mul,
     i64Modolus,
     i64Pow,
-    // short cuts
     i64Square,
-    i64Inc,
-    i64Dec,
     // StrConst,
     // StrIsEq,
     StrCmp,
@@ -35,10 +31,15 @@ enum Opcode {
     
     Call,
     
+    SetFlag,
+
     Drop,
     Pop,
     
-    Halt,
+    OpenFD,
+
+
+    Panic,
 
     Ret,
     Print
