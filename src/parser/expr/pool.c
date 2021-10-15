0c8837b3fa0ebcd8f90fadec02e057d1cc93c970
Skarlett <360d@pm.me>
2021-10-15 08:17:55 -0500

    added `ExprPool` to better allocate resources

--- src/parser/expr/pool.c
+++ src/parser/expr/pool.c
@@ -1,25 +1,103 @@
+#include "../../prelude.h"
 #include "../lexer/lexer.h"
 #include "expr.h"
+#include "pool.h"
 #include <stdint.h>
-#include "../../prelude.h"
+#include <stdbool.h>
+#include <stdlib.h>
 
-struct ExprPool {
-    usize capacity;
-    usize length;
-    struct Expr *heap_buf;
-};
-
-const struct Expr * pool_get(
-    const struct ExprPool * pool,
-    unsigned long int idx
-){
-    return pool->heap_buf + (idx * sizeof(struct Expr));
+const struct Expr * pool_get(const struct ExprPool * pool, usize idx)
+{
+    if (pool->capacity > idx)
+        return 0;
+    
+    return pool->heap_buf + sizeof(struct Expr) * idx;
 }
 
-uint32_t pool_push() {
+struct Expr * _pool_push(struct ExprPool *pool, const struct Expr e)
+{
+    if (pool->length >= pool->capacity)
+    {
+        /* is cache empty? */
+        if (pool->cached_ctr == 0) {
+            /* collect garbage/freed expression */
+            for (usize i=0; pool->capacity > i; i++)
+            {
+                if (pool->cached_ctr >= (uint8_t)CACHE_SZ)
+                    break;
+                    
+                else if (pool->heap_buf[i].free)
+                {
+                    pool->cached[pool->cached_ctr] = &pool->heap_buf[i];
+                    pool->cached_ctr += 1;
+                }
+            }
 
+            if (pool->cached_ctr > 0)
+            {
+                pool->cached_ctr -= 1;
+                return pool->cached[pool->cached_ctr];
+            }
+            return 0;
+        }
+
+        /* give free'd expr*/
+        else if (pool->cached_ctr > 0)
+        {
+            pool->cached_ctr -= 1;
+            return pool->cached[pool->cached_ctr];
+        }
+    }
+     
+    else if (pool->capacity > pool->length){
+        pool->heap_buf[pool->length] = e;
+        pool->length += 1;
+    
+        return &pool->heap_buf[pool->length - 1];    
+    }
+
+    return 0;
 }
 
-uint32_t pool_pop() {
+struct Expr * pool_push(struct ExprPool *pool, const struct Expr ex)
+{
+    struct Expr *phandle;
+    uint8_t phandle_i;
 
+    do {
+        phandle = _pool_push(pool, ex);
+        if (pool_realloc(pool) == -1 || phandle_i > 1)
+            return 0;
+        phandle_i += 1;
+    } while (phandle == 0);
+    
+    return phandle;
+}
+
+struct Expr * pool_pop(struct ExprPool *pool)
+{
+    if (0 >= pool->length)
+        return 0;
+    
+    pool->length -= 1;
+    pool->heap_buf[pool->length].free = true;
+    
+    return &pool->heap_buf[pool->length];
+}
+
+
+int8_t pool_realloc(struct ExprPool *pool) {
+    struct Expr * old_heap;
+
+    pool->capacity *= 2;
+    old_heap = pool->heap_buf;
+
+    pool->heap_buf = realloc(pool->heap_buf, pool->capacity * sizeof(struct Expr));
+
+    if (pool->heap_buf == 0) {
+        pool->heap_buf = old_heap;
+        return -1;
+    }
+
+    return 0;
 }
