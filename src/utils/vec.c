bc1a1faacf7ec608032052868beb10ec37744ab3
Skarlett <360d@pm.me>
2021-11-04 20:46:02 -0500

    add state flags for Vec, and add a free function

--- src/utils/vec.c
+++ src/utils/vec.c
@@ -4,12 +4,19 @@
 #include <string.h>
 #include "vec.h"
 
+#define VEC_STATE_INITALIZED 1
+#define VEC_STATE_FREED 2
+
 int8_t init_vec(struct Vec *vec, usize capacity, uint16_t type_sz) {
+    if (vec->state != 0)
+        return -1;
+    
     vec->type_sz = type_sz;
     vec->base = calloc(capacity, type_sz);
     vec->head = vec->base;
     vec->capacity = capacity;
     vec->len = 0;
+    vec->state = VEC_STATE_INITALIZED;
     
     if (vec->base == 0)
         return -1;
@@ -18,6 +25,9 @@ int8_t init_vec(struct Vec *vec, usize capacity, uint16_t type_sz) {
 
 int8_t vec_realloc(struct Vec *vec)
 {
+    if (vec->state != VEC_STATE_INITALIZED || vec->base == 0)
+        return -1;
+    
     void * ret = realloc(vec->base, (2 * vec->capacity) * vec->type_sz);
     
     if (ret == 0)
@@ -28,14 +38,26 @@ int8_t vec_realloc(struct Vec *vec)
     return 0;
 }
 
+int8_t vec_free(struct Vec *vec) {
+    if (vec->state != VEC_STATE_INITALIZED || vec->base == 0)
+        return -1;
+    
+    free(vec->base);
+    vec->state = VEC_STATE_FREED;
+    return 0;
+}
+
 void * vec_push(struct Vec *vec, void *src)
 {
     void * ret;
+    if (vec->state != VEC_STATE_INITALIZED || vec->base == 0)
+        return 0;
     if ((vec->head - vec->base) + vec->type_sz > (vec->type_sz * vec->capacity) && vec_realloc(vec) == -1)
         return 0;
     
     if (memcpy(vec->head, src, vec->type_sz) == 0)
         return 0;
+    
     ret = vec->head;
 
     vec->len += 1;
--- src/utils/vec.h
+++ src/utils/vec.h
@@ -1,7 +1,11 @@
 #ifndef _HEADER__VEC__
 #define _HEADER__VEC__
 #include <stdint.h>
+#include <sys/types.h>
 #include "../prelude.h"
+
+#define VEC_STATE_INITALIZED 1
+#define VEC_STATE_FREED 2
 
 struct Vec {
     void *base;
@@ -9,6 +13,8 @@ struct Vec {
     uint16_t type_sz;
     usize capacity;
     usize len;
+
+    uint8_t state;
 };
 
 int8_t init_vec(struct Vec *vec, usize capacity, uint16_t type_sz);
