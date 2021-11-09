505f4092f5702cd9f2ca2ddd22f80c825af4db03
Skarlett <360d@pm.me>
2021-11-09 07:24:01 -0600

    `Vec.capacity` & `Vec.type_sz` now accept `size_t` instead of `usize`

--- src/utils/vec.h
+++ src/utils/vec.h
@@ -1,25 +1,24 @@
 #ifndef _HEADER__VEC__
 #define _HEADER__VEC__
+#include <stddef.h>
 #include <stdint.h>
 #include <sys/types.h>
 #include "../prelude.h"
-
-#define VEC_STATE_INITALIZED 1
-#define VEC_STATE_FREED 2
 
 struct Vec {
     void *base;
     void *head;
     uint16_t type_sz;
-    usize capacity;
-    usize len;
+    size_t capacity;
+    size_t len;
 
     uint8_t state;
 };
 
-int8_t init_vec(struct Vec *vec, usize capacity, uint16_t type_sz);
+int8_t init_vec(struct Vec *vec, size_t capacity, size_t type_sz);
 int8_t vec_realloc(struct Vec *vec);
 void * vec_push(struct Vec *vec, void *src);
 void * vec_index(struct Vec *vec, usize idx);
+int8_t vec_free(struct Vec *vec);
 
 #endif
