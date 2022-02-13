2f3458ff27fc2b8d9aedc2058f83c9ec1d3c933a
Skarlett <360d@pm.me>
2022-02-13 00:15:00 -0600

    add compile-time token lists & todo

--- src/parser/predict.c
+++ src/parser/predict.c
@@ -1,81 +1,171 @@
+/*
+**
+**
+*/
+
 #include <stdint.h>
 #include <string.h>
-#include "../lexer/lexer.h"
-#include "expr.h"
-#include "utils.h"
+#include "lexer/lexer.h"
+#include "private.h"
 
-//TODO:
-//  - Rules for `FOR` & `WHILE` must follow an `(`
+/* TODO: Rules for `FOR`, `WHILE`, `IF` must follow an `(` */
+/* TODO: `IF`, `WHILE` signatures cannot */
+/*       contain delimiters, or keywords */
+/* TODO: ensure the following constrains on struct init */
+/*      WORD { WORD=.., } */
+/* TODO: ensure the correct delimiter */
+/*      is choosen for the current group */
+/* TODO: allow for `{x;;}` but not `[x,,]` */
+/* TODO: ensure when `ELSE` can be used */
+/* TODO: limit delimiters in index access >3 */
+/* TODO: import paths only accept WORD/DOT until delim */
+
+/* NOTE: DOT token is excluded here because */
+/* because it can't be applied to integers */
+/* unlike the other operators which can be applied */
+/* to groupings, words & integers */
+#define _EX_BIN_OPERATOR \
+    ADD, MUL, SUB, DIV, POW, MOD,        \
+    PIPE, AMPER, OR, AND,                \
+    LT, LTEQ, SHL,                       \
+    GT, GTEQ, SHR                        \
+    //MINUSEQ, PLUSEQ
+    // TILDE, NOT
+#define _EX_BIN_OPERATOR_LEN 16
 
 
-enum Lexicon _PV_default[] = {
-  _EX_EXPR,
-  0
-};
-#define _PV_default_len 9
+#define _EX_UNARY_OPERATOR TILDE, NOT
+#define _EX_UNARY_OPERATOR_LEN 2
 
-static enum Lexicon _PV_int[] = {
-  _EX_BIN_OPERATOR,
-  _EX_CLOSE_BRACE,
-  _EX_DELIM,
-  0
-};
-#define _PV_int_len 26
+#define _EX_DELIM COMMA, COLON, SEMICOLON
+#define _EX_DELIM_LEN 3
 
-static enum Lexicon _PV_word[] = {
-  _EX_BIN_OPERATOR,
-  DOT,
-  _EX_ASN_OPERATOR,
-  _EX_CLOSE_BRACE,
-  PARAM_OPEN,
-  BRACKET_OPEN,
-  _EX_DELIM,
-  0
-};
-#define _PV_word_len 35
+#define _EX_ASN_OPERATOR \
+    EQUAL, PLUSEQ, MINUSEQ, \
+    BANDEQL, BOREQL, BNEQL
+#define EX_ASN_OPERATOR_LEN 6
 
-static enum Lexicon _PV_str[] = {
-  _EX_BIN_OPERATOR,
-  DOT,
-  _EX_CLOSE_BRACE,
-  BRACKET_OPEN,
-  _EX_DELIM,
-  0
-};
-#define _PV_str_len 28
+#define _EX_OPEN_BRACE PARAM_OPEN, BRACE_OPEN, BRACKET_OPEN
+#define _EX_CLOSE_BRACE PARAM_CLOSE, BRACE_CLOSE, BRACKET_CLOSE
+#define _EX_BRACE_LEN 3
+
+#define _EX_DATA STRING_LITERAL, WORD, INTEGER
+#define _EX_DATA_LEN 3
+
+#define _EX_EXPR \
+  _EX_DATA,                                     \
+  _EX_OPEN_BRACE,                               \
+  _EX_UNARY_OPERATOR
+
+#define _EX_EXPR_LEN                            \
+  _EX_DATA_LEN                                  \
+  + _EX_BRACE_LEN                               \
+  + _EX_UNARY_OPERATOR_LEN
+
+
+/* NOTE:
+ * ELSE is not included,
+ * because it needs special checks
+*/
+#define _EX_KEYWORD                             \
+    IF, FUNC_DEF, FROM,                         \
+    IMPORT, STRUCT, FOR, WHILE, RETURN
+
+#define _EX_KEYWORD_LEN 8
+
+#define _PV_DEFAULT _EX_EXPR
+#define _PV_DEFAULT_LEN _EX_EXPR_LEN
+
+#define _PV_INT                                 \
+  _EX_BIN_OPERATOR,                             \
+  _EX_CLOSE_BRACE,                              \
+  _EX_DELIM
+
+#define _PV_INT_LEN                             \
+  _EX_BIN_OPERATOR_LEN                          \
+  + _EX_BRACE_LEN                               \
+  + _EX_DELIM_LEN
+
+#define _PV_WORD                                \
+  _EX_BIN_OPERATOR,                             \
+  _EX_ASN_OPERATOR,                             \
+  _EX_DELIM,                                    \
+  _EX_CLOSE_BRACE,                              \
+  PARAM_OPEN,                                   \
+  BRACKET_OPEN,                                 \
+  DOT
+
+#define _PV_WORD_LEN                            \
+  _EX_BIN_OPERATOR_LEN                          \
+  + _EX_ASN_OPERATOR_LEN                        \
+  + _EX_DELIM_LEN                               \
+  + _EX_BRACE_LEN                               \
+  + 3
+
+#define _PV_STR                                 \
+  _EX_BIN_OPERATOR,                             \
+  _EX_CLOSE_BRACE,                              \
+  _EX_DELIM,                                    \
+  BRACKET_OPEN,                                 \
+  DOT
+
+#define _PV_STR_LEN                             \
+  _EX_BIN_OPERATOR_LEN                          \
+  + _EX_BRACE_LEN                               \
+  + _EX_DELIM_LEN                               \
+  + 2
+
+#define _PV_DOT WORD
+#define _PV_DOT_LEN 1
+
+#define _PV_CLOSE_PARAM                         \
+  _EX_BIN_OPERATOR,                             \
+  _EX_CLOSE_BRACE,                              \
+  _EX_DELIM,                                    \
+  DOT,                                          \
+  BRACKET_OPEN,                                 \
+  PARAM_OPEN
+
+#define _PV_CLOSE_PARAM_LEN                     \
+  _EX_BIN_OPERATOR                              \
+  + _EX_BRACE_LEN                               \
+  + _EX_DELIM_LEN                               \
+  + 3
+
+/* throw unexpected token */
+int8_t is_illegal_delimiter(const struct Parser *state) {
+  struct Group *ghead = group_head(state);
+  const struct Token *gmod = group_modifier(state, ghead);
+
+  //TODO: add to predict.c
+  //if (ghead->type != CodeBlock)
+  //{
+  //  if(is_delimiter(next->type))
+  //    return -1;
+  //}
+
+  return \
+    gmod->type == WhileCond
+    || gmod->type == IfCond
+    || (ghead->type == _IdxAccess && ghead->delimiter_cnt > 2)
+    /* check for correct delimiter*/
+    // TODO: Handle in predict.c
+    //|| (gmod->type == _IdxAccess && current->type != COLON)
+    //|| ((ghead->type == TupleGroup || ghead->type == ListGroup) && current->type != COMMA)
+    //|| (ghead->type == CodeBlock && current->type != SEMICOLON)
+    ;
+}
 
 //TODO probably deserves its own mode
-static enum Lexicon _PV_if[] = {
-  PARAM_OPEN,
-  0
-};
+/* static enum Lexicon _PV_if[] = { */
+/*   PARAM_OPEN, */
+/*   0 */
+/* } */;
 
-#define _PV_if_len 2
+/* #define _PV_if_len 2 */
 
-enum Lexicon _PV_dot[] = {
-  WORD,
-  0
-};
-#define _PV_dot_len 2
-
-enum Lexicon exp_close_param[] = {
-  _EX_BIN_OPERATOR,
-  _EX_CLOSE_BRACE,
-  DOT,
-  PARAM_OPEN,
-  BRACKET_OPEN,
-  _EX_DELIM,
-  0
-};
-#define _PV_close_param_len 29
-
-enum Lexicon _PV_kw[] = {
-  IF,
-  FUNC_DEF,
-  IMPORT,
-  0
-};
-#define _PV_kw_len 4
+//#define _PV_KEYWORD _EX_KEYWORD
+//#define _PV_KEYWORD_LEN _EX_KEYWORD_LEN
 
 enum Lexicon _PV_import_init[] = {
   WORD,
@@ -83,6 +173,7 @@ enum Lexicon _PV_import_init[] = {
   0
 };
 #define _PV_import_init_len 3
+
 
 enum Lexicon _PV_import_word[] = {
   DOT,
@@ -102,12 +193,19 @@ uint16_t lex_arr_len(enum Lexicon *arr)
   return i;
 }
 
+bool can_use_else(enum Lexicon output_head) {
+  return output_head == IfBody;
+}
+
+bool can_use_keyword(enum Lexicon op_head) {
+  return is_open_brace(op_head);
+}
+
 /*
  * Write `_EX_EXPR` into state->expecting 
  */
 void init_expect_buffer(struct Previsioner *state)
-{   
-  
+{
     memcpy(state->buffer, _PV_default, sizeof(enum Lexicon) * 8);
     state->buffer[9] = 0;
     /* cast removes cc warning */
@@ -120,16 +218,12 @@ bool can_addon_keywords(struct Parser *state)
   const struct Token *token;
   struct Group *ghead = group_head(state);
 
-
   if(ghead->operator_idx > 0)
     token = state->operator_stack[ghead->operator_idx-1];
 
-  
   /* if open brace */
   if(is_open_brace(ophead->type))
     return  == 0;
-
-
 
   return true;
 }
