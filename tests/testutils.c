59b0d77d2a90ab79dee480148b8f829b82fdd930
Skarlett <360d@pm.me>
2022-02-13 18:08:00 -0600

    mv balance functions to `test/testutils.c`

--- tests/testutils.c
+++ tests/testutils.c
@@ -1,13 +1,92 @@
 #include "CuTest.h"
 #include "../src/parser/lexer/lexer.h"
 #include "../src/parser/lexer/debug.h"
-#include "../src/prelude.h"
 #include <stdint.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
 
 #define FMT_STR "%s\nexpected: \n%s\ngot: \n%s\nsrc: \"%s\""
+
+int8_t inner_balance(enum Lexicon tokens[], uint16_t *tokens_ctr, enum Lexicon current) {
+    //TODO: check for int overflow & buffer
+    enum Lexicon inverted;
+
+    if (is_close_brace(current))
+    {
+        inverted = invert_brace_tok_ty(current);
+        if (inverted == TOKEN_UNDEFINED)
+          return -1;
+
+       /*
+            return 1 to stop iteration,
+            and return `is_balanced` as false (0)
+        */
+        else if (*tokens_ctr <= 0)
+          return 1;
+
+        else if (tokens[*tokens_ctr - 1] == inverted)
+          *tokens_ctr -= 1;
+    }
+
+    else if (is_open_brace(current)) {
+        if (*tokens_ctr >= BRACE_BUFFER_SZ) {
+            return -1;
+        }
+
+        *tokens_ctr += 1;
+        tokens[(*tokens_ctr)-1] = current;
+    }
+
+    return 0;
+}
+
+/*
+ *  function determines if an expression is unbalanced.
+ *  an expression can be unbalanced
+ *  if there is a nonmatching `[` / `(` / `{` character
+*/
+bool is_balanced(struct Token tokens[], uint16_t ntokens) {
+    enum Lexicon braces[BRACE_BUFFER_SZ];
+    uint16_t braces_ctr = 0;
+    int8_t ret;
+
+    for (uint16_t i=0; ntokens > i; i++){
+        ret = inner_balance(braces, &braces_ctr, tokens[i].type);
+        if (ret == -1)
+            return -1;
+
+        // immediately unbalanced
+        else if (ret == 1)
+            return 0;
+    }
+
+    return braces_ctr == 0;
+}
+
+/*
+    function determines if an expression is unbalanced.
+    an expression can be unbalanced
+    if there is a nonmatching `[` / `(` / `{` character
+*/
+bool is_balanced_by_ref(struct Token *tokens[], uint16_t ntokens) {
+    enum Lexicon braces[BRACE_BUFFER_SZ];
+    uint16_t braces_ctr = 0;
+
+    int8_t ret;
+
+    for (uint16_t i=0; ntokens > i; i++){
+        ret = inner_balance(braces, &braces_ctr, tokens[i]->type);
+
+        if (ret == -1)
+            return -1;
+
+        else if (ret == 1)
+            return 0;
+    }
+
+    return braces_ctr == 0;
+}
 
 bool seq_eql_ty(
     const struct Token tokens[],
