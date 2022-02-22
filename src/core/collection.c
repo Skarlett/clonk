9a1b1a6cd16a964594f40d646de3a125e1ba8342
Skarlett <360d@pm.me>
2022-02-21 20:10:03 -0600

    start onkstd

--- src/core/collection.c
+++ src/core/collection.c
@@ -1,12 +1,14 @@
 #include <stdint.h>
 #include <stdbool.h>
+#include <string.h>
+#include <malloc.h>
 
 #define ONK_LAZY_REF_LIMIT 8
 #define ONK_LAZY_SZ_LIMIT 512
 
 enum onk_collection_no {
   onkstd_collection_udef_t,
-  onkstd_collection_lazy_t,
+  onkstd_collection_shallow_t,
   onkstd_collection_norm_t
 };
 
@@ -20,20 +22,24 @@ struct _onk_collection_norm_t {
    and treat the totality of the collection
    as one sequential array.
 
-   Reallocate into heap
-   if size limit is exceeded.
+   # Reallocate into heap
+   # if size limit is exceeded.
 */
-struct _onk_collection_lazy_t {
+struct _onk_collection_shallow_t {
     /* {void *, } */
-    const void * base[ONK_LAZY_REF_LIMIT];
-    uint16_t sizes[ONK_LAZY_REF_LIMIT];
-};
+    const void ** base;
 
+    /* list of lengths */
+    uint16_t * sizes;
+
+    /* how many items inside base*/
+    uint16_t slices;
+};
 
 struct onkstd_collection_t {
     enum onk_collection_no type;
     union {
-        struct _onk_collection_lazy_t lazy;
+        struct _onk_collection_shallow_t lazy;
         struct _onk_collection_norm_t norm;
     } alloc;
 
@@ -42,28 +48,66 @@ struct onkstd_collection_t {
 };
 
 
-struct onk_iter_collection {
+struct onk_iter_collection_t {
     /* {void *, } */
     const struct onkstd_collection_t *self;
     uint16_t i;
 };
 
 
-struct onk_iter_collection iter_lazy_collection
+struct onk_iter_collection_t onk_iter_collection
 (const struct onkstd_collection_t *ptr)
 {
-    struct onk_iter_collection iter;
+    struct onk_iter_collection_t iter;
     iter.self = ptr;
     iter.i = 0;
     return iter;
 }
 
-
-bool onk_step_iter(struct onk_iter_collection *iter)
+/*
+  for the collection to be normalized
+  into a deep-copy.
+*/
+int8_t onk_collection_deep_copy(struct onkstd_collection_t *col)
 {
+    void * heap;
+    const void *slice;
+    uint16_t nslices = 0, size, ncompleted = 0;
+
+    if(col->type != onkstd_collection_shallow_t)
+        return 0;
+
+    heap = calloc(col->nitems+1, col->item_sz);
+    nslices = col->alloc.lazy.slices;
+
+    for (uint16_t i=0; nslices > i; i++)
+    {
+        size = col->alloc.lazy.sizes[i];
+        slice = col->alloc.lazy.base[i];
+
+        memcpy(
+            heap + (ncompleted * col->item_sz),
+            slice,
+            size * col->item_sz
+        );
+
+        ncompleted += size;
+    }
 
 }
 
+bool onk_step_iter(
+    struct onk_iter_collection *iter,
+    uint16_t i
+)
+{
+
+
+}
+
+
+void * onk_access_collection()
+{}
 
 
 // for (int i=0; onk_step_iter(&iter, i); i++)
--- src/core/int.c
+++ src/core/int.c
@@ -1,7 +1,9 @@
 #include <stdint.h>
+#include <stdarg.h>
 
 /*
   Add 2 unsigned 16bit integers within bounds
+  of its MAX & MIN values.
 */
 uint16_t onkstd_add_u16(uint16_t a, uint16_t b)
 {
@@ -19,4 +21,22 @@ uint16_t onkstd_sub_u16(uint16_t a, uint16_t b)
   if (b > a) return 0;
 
   return a - b;
+}
+
+
+uint16_t onkstd_sum_u16(uint16_t num[], uint16_t num_sz) {
+  uint16_t sum = 0;
+  for (uint16_t i = 0; num_sz > i; i++)
+  {
+    sum = onkstd_add_u16(sum, num[i]);
+
+    if(sum == UINT16_MAX)
+      break;
+  }
+  return sum;
+}
+
+uint16_t onkstd_mul_u16(uint16_t a, uint16_t b)
+{
+
 }
