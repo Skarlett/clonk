a5b30ad8900735fb0b187f59a0b5b38e584a37fb
Skarlett <360d@pm.me>
2022-04-18 09:53:49 -0500

    rename safe-u16 funcs to `onk_<op>_u16`

--- include/onkstd/int.h
+++ include/onkstd/int.h
@@ -1,5 +1,5 @@
 #include "clonk.h"
 
-uint16_t onkstd_add_u16(uint16_t a, uint16_t b);
-uint16_t onkstd_sub_u16(uint16_t a, uint16_t b);
-uint16_t onkstd_sum_u16(uint16_t num[], uint16_t num_sz);
+uint16_t onk_add_u16(uint16_t a, uint16_t b);
+uint16_t onk_sub_u16(uint16_t a, uint16_t b);
+uint16_t onk_sum_u16(uint16_t num[], uint16_t num_sz);
