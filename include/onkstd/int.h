025756fcdb3e67f4097df5b04f5e76011a23c707
Skarlett <360d@pm.me>
2022-04-19 08:12:18 -0500

    add overflow flags

--- include/onkstd/int.h
+++ include/onkstd/int.h
@@ -1,5 +1,7 @@
 #include "clonk.h"
 
-uint16_t onk_add_u16(uint16_t a, uint16_t b);
-uint16_t onk_sub_u16(uint16_t a, uint16_t b);
-uint16_t onk_sum_u16(uint16_t num[], uint16_t num_sz);
+
+bool onk_can_add_u16(uint16_t a, uint16_t b);
+uint16_t onk_add_u16(uint16_t a, uint16_t b, bool *overflow);
+uint16_t onk_sub_u16(uint16_t a, uint16_t b, bool *overflow);
+uint16_t onk_sum_u16(uint16_t num[], uint16_t num_sz, bool *overflow);
