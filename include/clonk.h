3d527df16d4af41307c609dc9e7bae6bb94ed56d
Skarlett <360d@pm.me>
2022-07-23 15:37:42 -0500

    better x64 check

--- include/clonk.h
+++ include/clonk.h
@@ -12,7 +12,8 @@
 #include <sys/types.h>
 #include <stdbool.h>
 
-#define ONK_VERSION "0.0.4"
+#define ONK_VERSION "0.0.5"
+
 // -DINCLUDE_TESTS 1
 #define ONK_INCLUDE_TESTS 0
 
@@ -27,7 +28,7 @@
 
 // Bitness checks borrowed from
 // https://stackoverflow.com/a/12338526/201787
-#if _WIN64 || ( __GNUC__ && __x86_64__ )
+#if _WIN64 || ( __GNUC__ && INTPTR_MAX == INT64_MAX)
 
 #define SYS_ARCH_PRIVATE_DEFINITION_64() 1
 #define SYS_ARCH_PRIVATE_DEFINITION_32() 0
@@ -35,12 +36,12 @@
 #define SYS_ARCH_IF_64_BIT( x64, x86, x16 ) (x64)
 #define onk_str_to_isize strtoll
 #define onk_str_to_usize strtoull
-#define onk_isize_sz sizeof(int64_t)
+#define onk_isize_sz sizeof()
 #define onk_usize_sz sizeof(uint64_t)
 typedef uint64_t onk_usize;
 typedef int64_t onk_isize;
 
-#elif _WIN32 || __GNUC__
+#elif _WIN32 || (__GNUC__ && INTPTR_MAX == INT32_MAX
 #define SYS_ARCH_PRIVATE_DEFINITION_64() 0
 #define SYS_ARCH_PRIVATE_DEFINITION_32() 1
 #define SYS_ARCH_PRIVATE_DEFINITION_16() 0
@@ -57,8 +58,8 @@ typedef int32_t onk_isize;
 #define SYS_ARCH_PRIVATE_DEFINITION_32() 0
 #define SYS_ARCH_PRIVATE_DEFINITION_16() 1
 #define SYS_ARCH_IF_64_BIT_ONK_ELSE_TOKEN( x64, x86, x16 ) (x16)
-#define onk_str_to_isize strtol
-#define onk_str_to_usize strtoul
+#define onk_str_to_isize strtoint
+#define onk_str_to_usize strtouint
 #define onk_isize_sz sizeof(int16_t)
 #define onk_usize_sz sizeof(uint16_t)
 typedef uint16_t onk_usize;
@@ -101,7 +102,6 @@ typedef int16_t onk_isize;
 
 #define ONK_EOF 0
 #define onk_nop
-
 
 #define ONK_VEC_INC 256
 #define ONK_VEC_MAX 1 << 14
