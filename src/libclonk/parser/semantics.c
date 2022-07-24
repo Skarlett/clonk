ba87813c921157ab8ed79c0230d7381f9df21289
Skarlett <360d@pm.me>
2022-07-24 12:31:37 -0500

    remove old semantic checker

--- src/libclonk/parser/semantics.c
+++ src/libclonk/parser/semantics.c
@@ -48,6 +48,59 @@ const uint8_t ISET[] = {
   BINOP_LEN
 };
 
+void vframe_add_slice(
+    struct validator_frame_t *f,
+    const enum onk_lexicon_t *slice,
+    uint16_t len
+){
+    f->slices[f->nslices] = slice;
+    f->islices[f->nslices] = len;
+    f->nslices += 1;
+}
+
+enum onk_lexicon_t vframe_add_delimiter(struct onk_parser_state_t * state)
+{
+  struct onk_parse_group_t *ghead = group_head(state);
+  const struct onk_token_t *gmod;
+  // wtf?
+  //const struct onk_token_t *gmod = group_modifier(state, ghead);
+
+  switch (ghead->type) {
+    /* case onk_idx_op_token: */
+    /*   if(ghead->delimiter_cnt > 2) */
+    /*     return 0; */
+    /*   return ONK_COMMA_TOKEN; */
+
+    case onk_list_group_token:
+      gmod = group_modifier(state, ghead);
+
+      if (gmod && gmod->type == onk_idx_op_token)
+      {
+        if (2 > ghead->delimiter_cnt)
+          return ONK_COLON_TOKEN;
+        else return 0;
+      }
+      else return ONK_COMMA_TOKEN;
+
+    case onk_struct_group_token:
+      return ONK_COMMA_TOKEN;
+
+    case onk_tuple_group_token:
+      return ONK_COMMA_TOKEN;
+
+    case onk_map_group_token:
+      if(ghead->delimiter_cnt % 2 == 0)
+        return ONK_COLON_TOKEN;
+      return ONK_COMMA_TOKEN;
+
+    case onk_code_group_token:
+      return ONK_SEMICOLON_TOKEN;
+
+    default:
+      return 0;
+  }
+}
+
 // expects unit
 int8_t get_precedence_expr(enum onk_lexicon_t unit)
 {
@@ -83,7 +136,7 @@ void init_frame(
 /*
 ** deep clone data into *arr
 */
-bool _onk_semantic_check(
+bool onk_semantic_check(
   struct onk_parser_state_t *state,
   enum onk_lexicon_t current
 ){
@@ -333,6 +386,13 @@ int8_t build_next_frame(struct onk_parser_state_t *state)
 
   init_validator_frame(&frame, state);
 
+  if (ophead->type == onk_defsig_op_token)
+  {
+      state->expect[0] = ONK_WORD_TOKEN;
+      state->nexpect = 1;
+      return 0;
+  }
+
   // def foo(x, y)
   //         ^
   if(gmod && ((gmod->type == onk_defsig_op_token && ophead->type == ONK_PARAM_OPEN_TOKEN)
@@ -432,15 +492,13 @@ int8_t build_next_frame(struct onk_parser_state_t *state)
       vframe_add_slice(&frame, EXPR, EXPR_LEN);
   }
 
-
   // word *
   //   ^  {
   //   |  [
-  //   |->^ operator
+  //   |->^ operator (making frame)
   //   current
   else if(expr_precedense != -1)
       vframe_add_operators(&frame, expr_precedense);
-
 
   else
   {
@@ -453,5 +511,6 @@ int8_t build_next_frame(struct onk_parser_state_t *state)
       vframe_add_slice(&frame, KWORD_BLOCK, KWORD_BLOCK_LEN);
 
   state->nexpect = onk_semantic_compile(&frame, state);
+  return 0;
 }
 
