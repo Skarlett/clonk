47f6c8417200fe6d763ac8a4dff50effcd643e8a
Skarlett <skarlett@github.com>
2022-12-19 02:16:27 -0600

    rewrite it into rust

--- include/onkstd/merge_sort.h
+++
@@ -1,9 +0,0 @@
-#ifndef __ONK_SORT_H__
-#define __ONK_SORT_H__
-#include "lexer.h"
-#include <stdint.h>
-
-void onk_merge_sort_u16(uint16_t *arr, uint16_t l, uint16_t r);
-void onk_bubblesort_u16(uint16_t *arr, uint16_t n);
-void onk_bubblesort_lexarr(enum onk_lexicon_t *arr, uint16_t n);
-#endif
